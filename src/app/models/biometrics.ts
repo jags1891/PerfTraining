657567a81424283fb2036d7ea74aa8718849ff01
jags1891 <jagadeesh.bandlamudi@gmail.com>
2020-10-22 00:58:27 -0700

    Auto-generated commit

--- src/app/models/biometrics.ts
+++
@@ -1,6 +0,0 @@
-export class Biometrics {
-  HRV: number;
-  restingHeartRate: number;
-  fastingBloodSugar: number;
-  bloodPressure: number;
-}
--- src/app/models/coach.ts
+++
@@ -1,8 +0,0 @@
-export class Coach {
-  id: number;
-  firstName: string;
-  lastName: string;
-  name: string;
-  email: string;
-}
-  
